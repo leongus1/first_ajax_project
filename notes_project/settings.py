92dc4b00894848c9e163cec3e15f01857a1589d8
Ubuntu <ubuntu@ip-172-31-40-185.us-east-2.compute.internal>
2020-11-05 21:21:38 +0000

    get server ready

--- notes_project/settings.py
+++ notes_project/settings.py
@@ -23,9 +23,9 @@ BASE_DIR = os.path.dirname(os.path.dirname(os.path.abspath(__file__)))
 SECRET_KEY = '$0wo^ra&(kje$3r)lwvuk_1071#mozaw86048x+$#i_a$w!&ib'
 
 # SECURITY WARNING: don't run with debug turned on in production!
-DEBUG = True
+DEBUG = False
 
-ALLOWED_HOSTS = []
+ALLOWED_HOSTS = ['3.21.162.36']
 
 
 # Application definition
@@ -119,3 +119,4 @@ USE_TZ = True
 # https://docs.djangoproject.com/en/2.2/howto/static-files/
 
 STATIC_URL = '/static/'
+STATIC_ROOT = os.path.join(BASE_DIR, "static/")
