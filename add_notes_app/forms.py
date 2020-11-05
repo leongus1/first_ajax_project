8710379eb79357c5b477a9084824530bec7b8beb
leongus1 <leongus1@gmail.com>
2020-11-05 14:40:17 -0800

    latest local copy

--- add_notes_app/forms.py
+++ add_notes_app/forms.py
@@ -1,6 +1,6 @@
 from django import forms
 from .models import Notes
-from django.utils.translation import gettext_lazy as _
+# from django.utils.translation import gettext_lazy as _
 
 
 class CreateNote(forms.ModelForm):
